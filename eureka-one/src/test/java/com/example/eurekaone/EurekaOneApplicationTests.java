51fd5eedf4d7f014f471de840eb900dd2137bde7
김도희 <rlaehgml0125@gmail.com>
2021-07-09 16:56:10 +0900

    Initial commit

---
+++ eureka-one/src/test/java/com/example/eurekaone/EurekaOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
